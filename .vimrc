73d735cd7c7cd165e1fe928635e8e524605f514f
crab <crabtw@gmail.com>
2008-11-28 15:26:45 +0800

    home config

---
+++ .vimrc
@@ -0,0 +1,17 @@
+syntax on
+
+set cindent
+set expandtab
+set softtabstop=4
+set shiftwidth=4
+"set nowrap
+
+set fileformat=unix
+set fileformats=unix,dos,mac
+
+set encoding=utf-8
+set fileencoding=utf-8
+set fileencodings=ucs-bom,utf-8,big5,latin1
+
+colorscheme torte
+set hlsearch
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,37 @@
+import XMonad
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ToggleLayouts
+
+import XMonad.Util.Run (spawnPipe)
+
+import qualified Data.Map as M
+import System.IO (hPutStrLn)
+
+myPP bar = xmobarPP {
+            ppOutput = hPutStrLn bar
+           }
+
+myLayout = toggleLayouts (noBorders Full) defaultLayout
+    where defaultLayout = avoidStruts $
+                          smartBorders $
+                          layoutHook defaultConfig
+
+myKeys x = M.union (keys defaultConfig x) (M.fromList (newKeys x))
+    where newKeys x = [((modMask x, xK_f), sendMessage ToggleLayout)]
+
+myManageHook = manageDocks <+> manageHook defaultConfig
+
+main = do xmobar <- spawnPipe "xmobar"
+          xmonad $ defaultConfig {
+            layoutHook = myLayout,
+            logHook = dynamicLogWithPP $ myPP xmobar,
+            keys = myKeys,
+            modMask = mod4Mask,
+            manageHook = myManageHook,
+            startupHook = setWMName "LG3D"
+          }
