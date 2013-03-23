28c2f217ecdf2d18b17c1d24ab1c8db69e950960
crab <crabtw@gmail.com>
2013-03-24 00:36:32 +0800

    update ranger config

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -6,3 +6,13 @@ class extract(Command):
         for f in cwd.get_selection():
             self.fm.run(['/usr/bin/aunpack', f.basename])
             cwd.mark_item(f, False)
+
+class conv2mkv(Command):
+    def execute(self):
+        cwd = self.fm.env.cwd
+        for f in cwd.get_selection():
+            out = f.basename + '.mkv'
+            proc = self.fm.run(['/usr/bin/ffmpeg', '-i', f.basename, '-vcodec', 'copy', '-acodec', 'copy', out])
+            if proc.wait() != 0:
+                self.fm.run(['/usr/bin/mencoder', '-o', out, '-ovc', 'copy', '-oac', 'copy', '-of', 'lavf', '-lavfopts', 'format=matroska', f.basename])
+            cwd.mark_item(f, False)
