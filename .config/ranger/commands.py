91571462c36ef3d3d87033514fcbcbb83fcbd3c7
crab <crabtw@gmail.com>
2015-04-19 01:13:45 +0800

    update ranger config

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -2,17 +2,17 @@ from ranger.api.commands import *
 
 class extract(Command):
     def execute(self):
-        cwd = self.fm.env.cwd
-        for f in cwd.get_selection():
-            self.fm.run(['/usr/bin/aunpack', f.basename])
-            cwd.mark_item(f, False)
+        fm = self.fm
+        for f in fm.thisdir.get_selection():
+            fm.run(['/usr/bin/aunpack', f.basename])
+            fm.thisdir.mark_item(f, False)
 
 class conv2mkv(Command):
     def execute(self):
-        cwd = self.fm.env.cwd
-        for f in cwd.get_selection():
+        fm = self.fm
+        for f in fm.thisdir.get_selection():
             out = f.basename + '.mkv'
-            proc = self.fm.run(['/usr/bin/ffmpeg', '-i', f.basename, '-map', '0',  '-vcodec', 'copy', '-acodec', 'copy', out])
+            proc = fm.run(['/usr/bin/ffmpeg', '-i', f.basename, '-map', '0',  '-vcodec', 'copy', '-acodec', 'copy', out])
             if proc.wait() != 0:
-                self.fm.run(['/usr/bin/mencoder', '-o', out, '-ovc', 'copy', '-oac', 'copy', '-of', 'lavf', '-lavfopts', 'format=matroska', f.basename])
-            cwd.mark_item(f, False)
+                fm.run(['/usr/bin/mencoder', '-o', out, '-ovc', 'copy', '-oac', 'copy', '-of', 'lavf', '-lavfopts', 'format=matroska', f.basename])
+            fm.thisdir.mark_item(f, False)
